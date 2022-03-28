3446d59f3d8aed357e5604c1f1d1961941c13299
imad-almansi <imadalmansi@yahoo.com>
2022-03-28 09:03:52 +0200

    replace url

--- replace.sh
+++ replace.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
-URL=$(curl 'https://roya.tv/api/livestream/ticket' \
-  -X 'POST' \
-  -H 'authority: roya.tv' \
-  -H 'content-length: 0' \
-  -H 'sec-ch-ua: " Not;A Brand";v="99", "Google Chrome";v="97", "Chromium";v="97"' \
-  -H 'accept: */*' \
+URL=$(curl 'https://ticket.roya.tv/api/livestream/ticket' \
+  -H 'authority: ticket.roya.tv' \
+  -H 'sec-ch-ua: " Not A;Brand";v="99", "Chromium";v="99", "Google Chrome";v="99"' \
+  -H 'accept: application/json, text/plain, */*' \
   -H 'dnt: 1' \
-  -H 'x-requested-with: XMLHttpRequest' \
   -H 'sec-ch-ua-mobile: ?0' \
-  -H 'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36' \
+  -H 'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36' \
   -H 'sec-ch-ua-platform: "Linux"' \
   -H 'origin: https://roya.tv' \
-  -H 'sec-fetch-site: same-origin' \
+  -H 'sec-fetch-site: same-site' \
   -H 'sec-fetch-mode: cors' \
   -H 'sec-fetch-dest: empty' \
-  -H 'referer: https://roya.tv/live-stream' | jq -r '.data.secured_url')
+  -H 'referer: https://roya.tv/' | jq -r '.data.secured_url')
 
 sed -i "/roya-live/c$URL" Imad.m3u
 
